68badec55940a92efa8855ea4a92e8be309bf21b
caishuwei <2925486150@qq.com>
2020-06-16 18:31:56 +0800

    房子涂色

---
+++ algorithm/src/main/java/com/caisw/algorithm/leetcode/Solution1473.java
@@ -0,0 +1,23 @@
+package com.caisw.algorithm.leetcode;
+
+/**
+ * 一排房子涂色，计算最小花费方案
+ * https://leetcode-cn.com/problems/paint-house-iii/
+ */
+public class Solution1473 {
+
+    /**
+     * 最终每个房子都必须被涂色
+     * @param houses 房子颜色，有可能已经染好色了
+     * @param cost 0房子序号，1颜色对应的花费
+     * @param m 总共n个房子
+     * @param n 总共n种颜色
+     * @param target 目标街区数量，连续相同颜色的房子组成一个街区
+     */
+    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+        //
+
+
+        return 0;
+    }
+}
